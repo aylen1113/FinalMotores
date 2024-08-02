5f73aa2870eb291cd3ded2c36ada05243eb07fd5
aylen1113 <aylenrociolopez31@gmail.com>
2024-08-02 20:39:47 -0300

    -

--- Assets/Scripts/EnemyControl.cs
+++ Assets/Scripts/EnemyControl.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 using UnityEngine.AI;
 
@@ -7,7 +8,7 @@ public class EnemyControl : MonoBehaviour
 {
     public Transform target;
     public float speed = 5f;
-    //public float stopDistance = 10f;
+    public float stopDistance = 20f;
     private NavMeshAgent agent;
 
     private int hp;
@@ -29,52 +30,59 @@ public class EnemyControl : MonoBehaviour
         target = GameObject.Find("Player").transform;
         Anim = GetComponent<Animator>();
 
-        if (Anim == null)
-        {
-            Debug.LogError("Animator component not found!");
-        }
+        Anim.SetBool("attack", false);
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Anim != null)
+
+        if (Vector3.Distance(transform.position, target.position) < stopDistance)
         {
-            //if (Vector3.Distance(transform.position, target.position) > stopDistance)
-            //{
             Anim.SetBool("move", true);
             agent.SetDestination(target.position);
-            //}
-            //else
-            //{
-            //    agent.ResetPath();
-            //}
         }
+        else
+        {
+            agent.ResetPath();
+            Anim.SetBool("move", false);
+        }
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Bullet"))
+        {
+            Destroy(other.gameObject);
+            Damage();
+        }
+
     }
 
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.CompareTag("Bullet"))
-        {
-            Destroy(collision.gameObject);
-            Damage();
-        }
-
+        
+    
         if (collision.gameObject.CompareTag("Player"))
         {
             Debug.Log("Player Hit");
 
+
+            Anim.SetBool("attack", true);
             if (playerHealth != null)
             {
                 playerHealth.TakeDamage(damageAmount);
 
-                if (Anim != null)
-                {
-                    Anim.SetBool("attack", true);
+       
                 }
-            }
+            
+             }
         }
-    }
+    
+
+
 
     public void Damage()
     {
